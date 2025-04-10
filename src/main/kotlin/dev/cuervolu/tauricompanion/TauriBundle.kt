dba31e88eb92a069c447288a939980a31efea664
Ángel Cuervo <angel.cuervo187@gmail.com>
2025-04-10 09:54:05 -0400

    chore: update workspace

--- src/main/kotlin/dev/cuervolu/tauricompanion/TauriBundle.kt
+++ src/main/kotlin/dev/cuervolu/tauricompanion/TauriBundle.kt
@@ -1,13 +1,13 @@
-package com.github.cuervolu.tauricompanion
+package dev.cuervolu.tauricompanion
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.TauriBundle"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object TauriBundle : DynamicBundle(BUNDLE) {
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
---
+++ src/main/kotlin/dev/cuervolu/tauricompanion/project/TauriIcons.kt
@@ -0,0 +1,8 @@
+package dev.cuervolu.tauricompanion.project
+
+import com.intellij.openapi.util.IconLoader
+
+object TauriIcons {
+    @JvmField
+    val TAURI_ICON = IconLoader.getIcon("/icons/tauri.svg", TauriIcons::class.java)
+}
